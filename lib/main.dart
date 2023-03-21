e7ecc7a9acbb31e5e26a2cd5ddc115d8156be900
“Herlistan” <xvladosx@inbox.ru>
2023-03-21 10:17:24 +0300

    новая ветка, собираем и пушим изменения

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Общежитие КубГАУ'),
+      home: const MyHomePage(title: 'Общежития КубГАУ'),
     );
   }
 }
